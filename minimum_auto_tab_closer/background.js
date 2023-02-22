5ad5daf2e72bd948e5602f88cb5dc796e0272170
koshitake2m2 <blueling2m2dvp@gmail.com>
2023-02-23 03:21:52 +0900

    fix たまに閉じないバグ修正 2

--- minimum_auto_tab_closer/background.js
+++ minimum_auto_tab_closer/background.js
@@ -1,7 +1,13 @@
+const KEY = 'koshitake2m2_minimum_auto_tab_closer';
 chrome.runtime.onMessage.addListener(
     function (_message, sender) {
         setTimeout(function () {
-            chrome.tabs.remove(sender.tab.id);
+            console.log((new Date).toLocaleString(), 'onMessage', sender.tab);
+            chrome.tabs.remove(sender.tab.id).then(() => {
+                var newData = {};
+                newData[KEY] = 0;
+                chrome.storage.local.set(newData);
+            });
         }, 2000);
     }
 );
--- minimum_auto_tab_closer/content.js
+++ minimum_auto_tab_closer/content.js
@@ -1,1 +1,26 @@
 chrome.runtime.sendMessage({}, function () { });
+
+/**
+ * Retry closing tab
+ */
+const KEY = 'koshitake2m2_minimum_auto_tab_closer';
+const MAX_RETRY_COUNT = 3;
+const RETRY_DURATION_MS = 5000;
+chrome.storage.local.get(KEY).then((valuesByKey) => {
+    const retryCount = valuesByKey[KEY] ?? 0;
+    if (retryCount < MAX_RETRY_COUNT) {
+        console.log(`closing tab... retry count: ${retryCount}`);
+        const newData = {};
+        newData[KEY] = retryCount + 1;
+        setTimeout(function () {
+            chrome.storage.local.set(newData).then(() => {
+                location.reload();
+            });
+        }, RETRY_DURATION_MS);
+    } else {
+        var newData = {};
+        newData[KEY] = 0;
+        chrome.storage.local.set(newData);
+        console.log(`stop closing tab. retry count: ${MAX_RETRY_COUNT}`);
+    }
+});
